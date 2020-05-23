3527fa37ae18d9ccd0f0f7e51f13f0e27e86d68c
zygui_g <zygui@163.com>
2020-05-23 10:48:19 +0800

    第八天-测试FastDFS上传和下载(从阿里云服务器上)

---
+++ test-fastdfs/src/main/java/com/xuecheng/TestFastDFSApplication.java
@@ -0,0 +1,17 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @author zygui
+ * @date Created on 2020/5/23 10:13
+ */
+@SpringBootApplication
+public class TestFastDFSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestFastDFSApplication.class, args);
+    }
+}
---
+++ test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
@@ -0,0 +1,75 @@
+package com.xuecheng.test.fastdfs;
+
+import org.csource.common.MyException;
+import org.csource.fastdfs.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Description: 测试FastDFS
+ *
+ * @author zygui
+ * @date Created on 2020/5/23 10:11
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestFastDFS {
+
+    // 上传文件
+    @Test
+    public void testUpload() {
+        try {
+            // 加载fastdfs-client.properties文件
+            ClientGlobal.initByProperties("config/fastdfs-client.properties");
+            // 定义TrackerClient, 用于请求TrackerServer
+            TrackerClient trackerClient = new TrackerClient();
+            // 连接tracker
+            TrackerServer trackerServer = trackerClient.getConnection();
+            // 获取Storage
+            StorageServer storageServer = trackerClient.getStoreStorage(trackerServer);
+            // 创建storageClient
+            StorageClient1 storageClient1 = new StorageClient1(trackerServer, storageServer);
+            // 向storage服务器上传文件
+            // 本地上传文件的路径
+            String filePath = "F:/old.jpg";
+            // 上传成功后,拿到文件id
+            String fileId = storageClient1.upload_file1(filePath, "jpg", null);
+            System.out.println("fileId = " + fileId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 下载文件
+    @Test
+    public void testDownload() {
+        try {
+            // 加载fastdfs-client.properties文件
+            ClientGlobal.initByProperties("config/fastdfs-client.properties");
+            // 定义TrackerClient, 用于请求TrackerServer
+            TrackerClient trackerClient = new TrackerClient();
+            // 连接tracker
+            TrackerServer trackerServer = trackerClient.getConnection();
+            // 获取Storage
+            StorageServer storageServer = trackerClient.getStoreStorage(trackerServer);
+            // 创建storageClient
+            StorageClient1 storageClient1 = new StorageClient1(trackerServer, storageServer);
+            // 下载文件
+            // 文件id
+            String fileId = "group1/M00/00/00/rBFhCF7HnMiATcyJAAfd4SujPJk480_big.jpg";
+            byte[] bytes = storageClient1.download_file1(fileId);
+            // 使用输出流保存文件
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("C:/old.jpg"));
+            fileOutputStream.write(bytes);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
